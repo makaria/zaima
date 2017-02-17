62d400f59a59ff03105d7dba505d0aa603bcc436
Yudu Ban <banyudu@gmail.com>
2017-02-17 17:23:18 +0800

    use es6 style class instead of prototype

--- js/eventPage.js
+++ js/eventPage.js
@@ -6,14 +6,14 @@
 // })
 
 // xhr request
-var QueuedHandler = function () {
-  this.queue = [] // 请求队列
-  this.requestInProgress = false // 判断当前是否己有别的请求
-  this.retryDelay = 5 // 设置每次重新请求的时间，单位为秒
-}
+class QueuedHandler {
+  constructor() {
+    this.queue = [] // 请求队列
+    this.requestInProgress = false // 判断当前是否己有别的请求
+    this.retryDelay = 5 // 设置每次重新请求的时间，单位为秒
+  }
 
-QueuedHandler.prototype = {
-  request: function (method, url, callback, postVars, override) {
+  request(method, url, callback, postVars, override) {
     // 如果没有设置为覆盖模式，而且当前已经有别的请求
     if (this.requestInProgress && !override) {
       this.queue.push({
@@ -40,14 +40,14 @@ QueuedHandler.prototype = {
         if ((xhr.readyState !== 4) || false) return
         if (xhr.status === 200) {
           callback.success(xhr.responseText, url)
-            // 判断请求队列是否为空，如果不为空继续下一个请求
+          // 判断请求队列是否为空，如果不为空继续下一个请求
           that.advanceQueue(callback)
         } else if (xhr.status === 0) {
           callback.timeout(xhr.status)
           that.advanceQueue(callback)
         } else {
           callback.failure(xhr.status)
-            // 每过一定时间重新请求
+          // 每过一定时间重新请求
           setTimeout(function () {
             that.request(method, url, callback, postVars)
           }, that.retryDelay * 1000)
@@ -59,9 +59,9 @@ QueuedHandler.prototype = {
       if (method !== 'POST') postVars = null
       xhr.send(postVars)
     }
-  },
+  }
 
-  createXhrObject: function () {
+  createXhrObject() {
     var methods = [
       function () {
         return new XMLHttpRequest()
@@ -85,9 +85,9 @@ QueuedHandler.prototype = {
     }
 
     throw new Error('SimpleHandler: Could not create an XHR object.')
-  },
+  }
 
-  advanceQueue: function (callback) {
+  advanceQueue(callback) {
     if (this.queue.length === 0) {
       this.requestInProgress = false
       callback.complete()
@@ -101,31 +101,27 @@ QueuedHandler.prototype = {
 var myRequest = new QueuedHandler()
 
 // storage
-
-var ChromeHandler = function () {
-
-}
-
-ChromeHandler.prototype = {
-  set: function (obj, callback) {
+class ChromeHandler {
+  constructor() {}
+  set(obj, callback) {
     chrome.storage.sync.set(obj, function (data) {
       callback && callback(data)
     })
-  },
+  }
 
-  get: function (key, callback) {
+  get(key, callback) {
     chrome.storage.sync.get(key, function (data) {
       callback && callback(data)
     })
-  },
+  }
 
-  remove: function (key, callback) {
+  remove(key, callback) {
     chrome.storage.sync.remove(key, function (data) {
       callback && callback(data)
     })
-  },
+  }
 
-  push: function (key, value, callback) {
+  push(key, value, callback) {
     chrome.storage.sync.get(key, function (result) {
       var array = result[key] ? result[key] : []
       array.push(value)
@@ -135,9 +131,9 @@ ChromeHandler.prototype = {
         callback && callback(data)
       })
     })
-  },
+  }
 
-  setBadge: function (text, callback) {
+  setBadge(text, callback) {
     chrome.browserAction.setBadgeText({
       text: myChannel.online.toString()
     })
@@ -146,46 +142,46 @@ ChromeHandler.prototype = {
 }
 
 // channels
-var ChannelHandler = function () {
-  this.defaultChannels = [{
-    url: 'http://www.douyu.com/63375',
-    nickname: 'SteamParty',
-    title: '',
-    domain: 'douyu',
-    id: '63375',
-    alterId: null,
-    online: false,
-    hide: false, //暂时不想知道这个房间是否online。获取数据时忽略此房间
-    color: false, //自定义online颜色
-    data: null
-  }, {
-    url: 'http://live.bilibili.com/41515',
-    nickname: '有C',
-    title: '',
-    domain: 'bilibili',
-    id: '41515',
-    alterId: null,
-    online: false,
-    hide: false,
-    color: false,
-    data: null
-  }]
-  this.api = {
-    douyu: 'http://open.douyucdn.cn/api/RoomApi/room/',
-    bilibili: 'http://live.bilibili.com/live/getInfo?roomid='
+class ChannelHandler {
+  constructor() {
+    this.defaultChannels = [{
+      url: 'http://www.douyu.com/63375',
+      nickname: 'SteamParty',
+      title: '',
+      domain: 'douyu',
+      id: '63375',
+      alterId: null,
+      online: false,
+      hide: false, //暂时不想知道这个房间是否online。获取数据时忽略此房间
+      color: false, //自定义online颜色
+      data: null
+    }, {
+      url: 'http://live.bilibili.com/41515',
+      nickname: '有C',
+      title: '',
+      domain: 'bilibili',
+      id: '41515',
+      alterId: null,
+      online: false,
+      hide: false,
+      color: false,
+      data: null
+    }]
+    this.api = {
+      douyu: 'http://open.douyucdn.cn/api/RoomApi/room/',
+      bilibili: 'http://live.bilibili.com/live/getInfo?roomid='
+    }
+    this.online = 0
+    this.newChannel = null
+    this.channels = []
+    this.interval = 1000 * 60 * 30 //每隔一定时间间隔重新获取数据
+    this.recent = 1000 * 60 * 5 //如果最近刚获取过数据则不再请求
+    this.start = true
+    this.fetching = false
+    this.timestamp = 0
   }
-  this.online = 0
-  this.newChannel = null
-  this.channels = []
-  this.interval = 1000 * 60 * 30 //每隔一定时间间隔重新获取数据
-  this.recent = 1000 * 60 * 5 //如果最近刚获取过数据则不再请求
-  this.start = true
-  this.fetching = false
-  this.timestamp = 0
-}
 
-ChannelHandler.prototype = {
-  filter: function (key, text) {
+  filter(key, text) {
     if (text.match(/douyu/) || text.match(/live\.bilibili/)) {
       var reg = null
       var match = null
@@ -206,9 +202,9 @@ ChannelHandler.prototype = {
         }
       }
     }
-  },
+  }
 
-  getApiUrl: function (channel) {
+  getApiUrl(channel) {
     if (channel.apiUrl) {
       return channel.apiUrl
     } else {
@@ -219,9 +215,9 @@ ChannelHandler.prototype = {
         return channel.apiUrl
       }
     }
-  },
+  }
 
-  getTitle: function (channel) {
+  getTitle(channel) {
     var data = channel.data
     var url = channel.url
     if (url.match(/douyu/)) {
@@ -231,21 +227,21 @@ ChannelHandler.prototype = {
       channel.title = data.ANCHOR_NICK_NAME + data.ROOMTITLE
       this.setNickname(channel, data.ANCHOR_NICK_NAME)
     }
-  },
+  }
 
-  setNickname: function (channel, nickname) {
+  setNickname(channel, nickname) {
     if (channel.nickname === 'New' || !channel.nickname) {
       channel.nickname = nickname
     }
-  },
+  }
 
-  isNewChannel: function (channel) {
+  isNewChannel(channel) {
     var isNew = true
     var channels = this.channels
     var length = channels.length
     var data = channel.data
     var id = data.room_id || data.ROOMID
-      // 更改id&&别名id&&apiUrl中的id
+    // 更改id&&别名id&&apiUrl中的id
     if (id !== channel.id) {
       channel.alterId = channel.id
       channel.id = id
@@ -260,34 +256,34 @@ ChannelHandler.prototype = {
       this.channels.push(channel)
     }
     this.newChannel = null
-  },
+  }
 
-  generateChannel: function (value) {
+  generateChannel(value) {
     var id = this.filter('id', value)
     var domain = this.filter('domain', value)
     var channel = {
-        apiUrl: this.api[domain] + id,
-        nickname: 'New',
-        domain: domain,
-        id: id,
-        data: null
-      }
-      // 补全url，有必要吗？
+      apiUrl: this.api[domain] + id,
+      nickname: 'New',
+      domain: domain,
+      id: id,
+      data: null
+    }
+    // 补全url，有必要吗？
     if (domain === 'douyu') {
       channel.url = 'http://www.douyu.com/' + id
     } else if (domain === 'bilibili') {
       channel.url = 'http://live.bilibili.com/' + id
     }
     return channel
-  },
+  }
 
-  deleteChannel: function (index, callback) {
+  deleteChannel(index, callback) {
     var channels = this.channels
     channels.splice(index, 1)
     this.saveChannels(callback)
-  },
+  }
 
-  saveChannel: function (text, url, callback) { //找到对应的channel然后更新。考虑更改callback的方式以直接更新，不用找。
+  saveChannel(text, url, callback) { //找到对应的channel然后更新。考虑更改callback的方式以直接更新，不用找。
     var obj = JSON.parse(text)
     var data = obj.data,
       channel
@@ -305,7 +301,7 @@ ChannelHandler.prototype = {
         if (channel.apiUrl === url) {
           channel.data = data
           var id = data.room_id || data.ROOMID
-            // 更改id&&别名id&&apiUrl中的id
+          // 更改id&&别名id&&apiUrl中的id
           if (id !== channel.id) {
             channel.alterId = channel.id
             channel.id = id
@@ -317,9 +313,9 @@ ChannelHandler.prototype = {
         }
       }
     }
-  },
+  }
 
-  saveChannels: function (callback) {
+  saveChannels(callback) {
     var that = this
     var obj = {}
     var channels = JSON.parse(JSON.stringify(this.channels))
@@ -332,52 +328,52 @@ ChannelHandler.prototype = {
     chrome.storage.sync.set(obj, function (data) {
       callback && callback()
     })
-  },
+  }
 
-  addChannel: function (value, callback) { //TODO 无法同时添加多个？没有检验是否重复
+  addChannel(value, callback) { //TODO 无法同时添加多个？没有检验是否重复
     var channel = this.generateChannel(value)
     var url = this.getApiUrl(channel)
     this.newChannel = channel
     myRequest.request('GET', url, callback)
-  },
+  }
 
-  initChannels: function (callback) {
+  initChannels(callback) {
     var that = this
     chrome.storage.sync.get('channels', function (data) {
       that.channels = data['channels'] ? data['channels'] : that.defaultChannels
       callback && callback(data)
     })
-  },
+  }
 
-  fetchChannels: function (callback) {
+  fetchChannels(callback) {
     var that = this
     this.fetching = true
     this.initChannels(function (data) {
       var channels = that.channels
       var length = channels.length
-        // console.log(channels)
+      // console.log(channels)
       var channel
       for (var i = 0; i < length; i++) {
         channel = channels[i]
         var url = that.getApiUrl(channel)
-          // console.log(url)
+        // console.log(url)
         if (url) {
           myRequest.request('GET', url, callback)
         }
       }
     })
-  },
+  }
 
-  isOnline: function (channel) {
+  isOnline(channel) {
     var data = channel.data ? channel.data : {}
     if (data.room_status == 1 || data.LIVE_STATUS == 'LIVE') {
       channel.online = true
     } else {
       channel.online = false
     }
-  },
+  }
 
-  totalOnline: function (callback) {
+  totalOnline(callback) {
     this.online = 0
     var channels = this.channels
     var length = channels.length
@@ -390,17 +386,17 @@ ChannelHandler.prototype = {
     }
     this.setBadge()
     callback && callback()
-  },
+  }
 
-  setBadge: function (callback) {
+  setBadge(callback) {
     var that = this
     chrome.browserAction.setBadgeText({
       text: that.online.toString()
     })
     callback && callback()
-  },
+  }
 
-  isExciting: function (url, callback) {
+  isExciting(url, callback) {
     var naive = false
     var id = this.filter('id', url)
     if (id) {
@@ -418,21 +414,21 @@ ChannelHandler.prototype = {
       naive = 'none'
     }
     callback && callback(naive)
-  },
+  }
 
-  toggleExciting: function (url, callback) {
+  toggleExciting(url, callback) {
     // var naive = false
     var id = this.filter("id", url)
-      // var channels = this.channels
-      // var length = channels.length
-      // var channel, index
-      // for (var i = 0; i < length; i++) {
-      //   channel = channels[i]
-      //   if (channel.id === id || channel.alterId === id) {
-      //     naive = true
-      //     index = i
-      //   }
-      // }
+    // var channels = this.channels
+    // var length = channels.length
+    // var channel, index
+    // for (var i = 0; i < length; i++) {
+    //   channel = channels[i]
+    //   if (channel.id === id || channel.alterId === id) {
+    //     naive = true
+    //     index = i
+    //   }
+    // }
     var naive = this.channels.find((channel) => channel.id == id || channel.alterId == id)
     if (naive) {
       this.deleteChannel(index, callback.complete)
@@ -444,56 +440,56 @@ ChannelHandler.prototype = {
 
 var myChannel = new ChannelHandler()
 
-var Setting = function () {
-  this.showHidden = true //是否显示hide的房间
-  this.showOnline = false //只显示上线的房间
-  this.interval = 1000 * 60 * 30 //刷新时间间隔
-  this.disableNickname = false //不显示昵称
-  this.diableName = false //不显示主播名
-  this.disableExciting = false //不显示关注/已关注按钮
-  this.disableTitle = false //不显示房间名
-  this.customOnlineColor = false //自定义online的颜色，修改popup.css中的相应样式
-  this.customOfflineColor = false //自定义offline的颜色
-}
+class Setting {
+  constructor() {
+    this.showHidden = true //是否显示hide的房间
+    this.showOnline = false //只显示上线的房间
+    this.interval = 1000 * 60 * 30 //刷新时间间隔
+    this.disableNickname = false //不显示昵称
+    this.diableName = false //不显示主播名
+    this.disableExciting = false //不显示关注/已关注按钮
+    this.disableTitle = false //不显示房间名
+    this.customOnlineColor = false //自定义online的颜色，修改popup.css中的相应样式
+    this.customOfflineColor = false //自定义offline的颜色
+  }
 
-Setting.prototype = {
-  toggleShowHidden: function () {
+  toggleShowHidden() {
     this.showHidden = !this.showHidden
-  },
+  }
 
-  toggleShowOnline: function () {
+  toggleShowOnline() {
     this.showOnline = !this.showOnline
-  },
+  }
 
-  toggleDisableNickname: function () {
+  toggleDisableNickname() {
     this.disableNickname = !this.disableNickname
-  },
+  }
 
-  toggleDisableName: function () {
+  toggleDisableName() {
     this.disableName = !this.disableName
-  },
+  }
 
-  toggleDisableExciting: function () {
+  toggleDisableExciting() {
     this.disableExciting = !this.disableExciting
-  },
+  }
 
-  toggleDisableTitle: function () {
+  toggleDisableTitle() {
     this.disableTitle = !this.disableTitle
-  },
+  }
 
-  changeInterval: function (interval) {
+  changeInterval(interval) {
     this.interval = interval * 1000 //最小单位是秒
-  },
+  }
 
-  changeOnlineColor: function (color) {
+  changeOnlineColor(color) {
     this.customOnlineColor = color
-  },
+  }
 
-  changeOfflineColor: function (color) {
+  changeOfflineColor(color) {
     this.customOfflineColor = color
-  },
+  }
 
-  saveSetting: function () {
+  saveSetting() {
     var setting = {
       showHidden: this.showHidden,
       showOnline: this.showOnline,
@@ -511,9 +507,9 @@ Setting.prototype = {
       console.log("设置已保存！")
       console.log(data)
     })
-  },
+  }
 
-  initSetting: function () {
+  initSetting() {
     var that = this
     chrome.storage.sync.get('setting', function (data) {
       var setting = data.setting || {}
@@ -525,7 +521,6 @@ Setting.prototype = {
 }
 
 var mySetting = new Setting()
-
 // start
 var callbacks = {
   success: function (responseText, url) {
